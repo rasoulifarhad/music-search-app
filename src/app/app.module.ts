156ff8f8977370aab8397ccbeef55def45d7218a
Farhad rasouli <rasouli.farhad@gmail.com>
2023-11-28 00:49:12 +0330

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,35 @@ import { SearchComponent } from './search/search.component';
 import { ArtistComponent } from './artist/artist.component';
 import { AlbumComponent } from './album/album.component';
 import { TrackComponent } from './track/track.component';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from './auth.service';
+import { ProtectedComponent } from './protected/protected.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
+import { Routes } from '@angular/router';
+import { loggedInGuard } from './logged-in.guard';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent},
+  { path:'cotactus', redirectTo: 'contact'},
+  { path: 'login', component: LoginComponent},
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [loggedInGuard]
+
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
@@ -14,12 +43,19 @@ import { TrackComponent } from './track/track.component';
     SearchComponent,
     ArtistComponent,
     AlbumComponent,
-    TrackComponent
+    TrackComponent,
+    LoginComponent,
+    ProtectedComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
