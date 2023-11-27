8379ec3962dbc2946ca5a811129d64710dce9a29
Farhad rasouli <rasouli.farhad@gmail.com>
2023-11-27 21:54:56 +0330

    updated

--- src/app/spotify.service.ts
+++ src/app/spotify.service.ts
@@ -1,16 +1,46 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
 
+  static BASE_URL = "https://api.spotify.com/v1";
+
   constructor(public http: HttpClient) { }
 
-  searchTrack(query: string) {
-    let params : string = [`q=${query}`, `type=track`].join("&");
-    let queryUrl : string = `https://api.spotify.com/v1/search?${params}`;
-    return this.http.get(queryUrl);
+  getTrack(id : string) : Observable<any> {
+    return this.query(`/tracks/${id}`)
   }
+
+  searchTrack(query: string) {
+    return this.search(query, "track")
+  }
+
+  search(query: string, type: string) {
+    return this.query(`/search`, [`q=${query}`, `type=${type}`])
+  }
+
+  query(URL: string, params?: Array<string>) : Observable<any> {
+    let queryURL = `${SpotifyService.BASE_URL}${URL}`;
+    if(params) {
+      queryURL = `${queryURL}?${params.join("&")}`;
+    }
+
+    const apiKey = environment.spotifyApiKey;
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${apiKey}`
+    });
+
+    const options = {
+      headers : headers
+    }
+
+    return this.http.get(queryURL, options);
+  }
+
 }
--- src/app/track/track.component.ts
+++ src/app/track/track.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../spotify.service';
 
 @Component({
   selector: 'app-track',
   templateUrl: './track.component.html',
   styleUrls: ['./track.component.css']
 })
-export class TrackComponent {
+export class TrackComponent implements OnInit {
+
+  id?: string;
+  track?: any;
+
+  constructor(private spotify: SpotifyService) {
+
+  }
+  ngOnInit(): void {
+    if(this.id) {
+      this.spotify
+          .getTrack(this.id)
+          .subscribe((res: any) => this.renderTrack(res));
+}
+  }
+
+  renderTrack(res: any): void {
+    if(res) {
+      this.track = res;
+    }
+  }
+
+  back() : void {
+
+  }
 
 }
